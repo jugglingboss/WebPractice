02fc3a9c3674e50e4794bbc3c6793b828fe25ca7
Michael Babilya <mjbabilya@yahoo.com>
2020-02-04 22:12:07 -0500

    finished java script crash course

--- js_crash_course/main.js
+++ js_crash_course/main.js
@@ -235,5 +235,62 @@
 //const items = document.querySelectorAll('.item');
 //items.forEach((item) => console.log(item))
 
+//const ul = document.querySelector('.items');
+//ul.remove();
+//ul.lastElementChild.remove()
+//ul.firstElementChild.textContent = 'Hello'
+//ul.children[1].innerText = 'Mike'
+//ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
+
+//const btn = document.querySelector('.btn');
+//btn.style.background = 'red' //USE THIS IN HOME AUTOMATION AFTER SOMETHING IS CLICKED ON TO CHANGED THE BACKROUND COLOR
+
+// btn.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     //console.log('click');
+//     //console.log(e.target.className)
+//     document.querySelector('#my-form').style.background = '#ccc'
+//     document.querySelector('body').classList.add('bg-dark')
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+// });
+
+// btn.addEventListener('mouseover', (e) => {
+//     e.preventDefault();
+//     //console.log('click');
+//     //console.log(e.target.className)
+//     document.querySelector('#my-form').style.background = '#ccc'
+//     document.querySelector('body').classList.add('bg-dark')
+//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+// });
 
 
+const myFrom = document.querySelector('#my-form')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const msg = document.querySelector('.msg')
+const userList = document.querySelector('#users')
+
+
+myFrom.addEventListener('submit',onSubmit)
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    //console.log(nameInput.value)
+    if(nameInput.value === '' || emailInput.value === ''){
+        //alert('Please enter fields')
+        msg.classList.add('error')
+        msg.innerHTML = 'Please enter all fields';
+        setTimeout(() => msg.remove(), 3000)
+    } else{
+        //console.log('success');
+        const li = document.createElement('li');
+        li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`))
+
+        userList.appendChild(li);
+
+        //Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
