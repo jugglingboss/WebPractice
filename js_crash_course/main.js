7b40412091bde8724ad136b8136840452014a9bb
Michael Babilya <mjbabilya@yahoo.com>
2020-02-06 22:29:39 -0500

    starting new dashboard style website

--- js_crash_course/main.js
+++ js_crash_course/main.js
@@ -242,17 +242,17 @@
 //ul.children[1].innerText = 'Mike'
 //ul.lastElementChild.innerHTML = '<h1>Hello</h1>'
 
-//const btn = document.querySelector('.btn');
+const btn = document.querySelector('.btn');
 //btn.style.background = 'red' //USE THIS IN HOME AUTOMATION AFTER SOMETHING IS CLICKED ON TO CHANGED THE BACKROUND COLOR
 
-// btn.addEventListener('click', (e) => {
-//     e.preventDefault();
-//     //console.log('click');
-//     //console.log(e.target.className)
-//     document.querySelector('#my-form').style.background = '#ccc'
-//     document.querySelector('body').classList.add('bg-dark')
-//     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
-// });
+btn.addEventListener('click', (e) => {
+     e.preventDefault();
+     //console.log('click');
+     //console.log(e.target.className)
+     document.querySelector('#my-form').style.background = '#ccc'
+     document.querySelector('body').classList.add('bg-dark')
+     document.querySelector('.items').lastElementChild.innerHTML = '<h1>Hello</h1>'
+ });
 
 // btn.addEventListener('mouseover', (e) => {
 //     e.preventDefault();
